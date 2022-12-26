2c8dce6473bcd71c94ab960b284d61fb0cead96b
Matheus <matheusslg1@gmail.com>
2022-12-26 14:31:04 -0300

    fix: babel config and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
 import { View, Text } from "react-native";
 
 const App: React.FC = () => {
     return (
-        <View>
-            <Text>Wallet App</Text>
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Text>Carteira App</Text>
         </View>
     )
 }
 
-export { App };
+export default App;
