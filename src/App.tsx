aab90d9331e234514d176b895b949f89aa84e216
Matheus <matheusslg1@gmail.com>
2022-12-26 13:03:57 -0300

    ci: configuring the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App: React.FC = () => {
+    return (
+        <View>
+            <Text>Wallet App</Text>
+        </View>
+    )
+}
+
+export { App };
