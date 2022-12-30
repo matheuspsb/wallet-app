a3c339e8e647dcda527dc4ffe4d0aeb39f3bb3f2
Matheus <matheusslg1@gmail.com>
2022-12-30 14:20:50 -0300

    feat: styling input field

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Text } from 'react-native'
+
+import { Container } from './styles'
+
+const Input: React.FC = () => {
+  return (
+    <Container>
+        <Text>Input</Text>
+    </Container>
+  )
+}
+
+export default Input
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,72 @@
+import styled, { css } from "styled-components/native";
+import { TextInput, ViewProps } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+interface IContainerProps extends ViewProps {
+    hasError: boolean;
+    isFocused: boolean;
+    isFilled?: boolean;
+}
+
+interface ITextContainerProps extends ViewProps {
+    hasError: boolean;
+    isFocused: boolean;
+    isFilled?: boolean;
+}
+
+export const Container = styled.View`
+    margin-bottom: ${RFValue(10)}px ;
+
+    width: 85%;
+    height: ${RFValue(50)}px;
+
+    flex-direction: row;
+`
+
+export const IConContainer = styled.View<IContainerProps>`
+    width: ${RFValue(55)}px;
+    height: ${RFValue(50)}px;
+
+    justify-content: center;
+    align-items: center;
+
+    ${({ isFocused, isFilled }) => (isFocused || isFilled) && css`
+        border-bottom-width: ${RFValue(2)}px;
+        border-bottom-color: ${({ theme }) => theme.COLORS.BLUE1};
+    `}
+
+    ${({ hasError }) => hasError && css`
+       border-bottom-color : ${({ theme }) => theme.COLORS.RED};
+    `}
+
+    margin-right: ${RFValue(3)}px;
+
+    border-top-left-radius: ${RFValue(5)}px;
+    border-bottom-left-radius: ${RFValue(5)}px;
+
+    background-color: ${({ theme }) => theme.COLORS.GRAY5};
+`
+
+export const InputText = styled(TextInput) <ITextContainerProps>`
+    flex: 1;
+
+    font-size: ${RFValue(12)}px;
+    font-family: ${({ theme }) => theme.FONTS.POPPINSLIGHT} ;
+    color: ${({ theme }) => theme.COLORS.GRAY4};
+
+    border-top-right-radius: ${RFValue(5)}px;
+    border-bottom-right-radius: ${RFValue(5)}px;
+
+    background-color: ${({ theme }) => theme.COLORS.GRAY5};
+
+    ${({ isFocused, isFilled }) => (isFocused || isFilled) && css`
+        border-bottom-width: ${RFValue(2)}px;
+        border-bottom-color: ${({ theme }) => theme.COLORS.BLUE1};
+    `}
+
+    ${({ hasError }) => hasError && css`
+       border-bottom-color : ${({ theme }) => theme.COLORS.RED};
+    `}
+
+    padding: 0 ${RFValue(10)}px;
+`
